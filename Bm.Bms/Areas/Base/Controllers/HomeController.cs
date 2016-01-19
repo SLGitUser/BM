91b146719329b1ab41d0d07f6c67f4c7cd366862
david <zhang_david@aliyun.com>
2016-01-20 00:36:07 +0800

    feat<Bms>: + Index page

---
+++ Bm.Bms/Areas/Base/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bm.Areas.Base.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Base/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Bm.Bms/Areas/Biz/BizAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Bm.Areas.Biz
+{
+    public class BizAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Biz";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Biz_default",
+                "Biz/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}

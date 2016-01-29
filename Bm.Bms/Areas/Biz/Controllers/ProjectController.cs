5411070014b3a8f99d8a231a55c9fa0e00883923
zzyao <zzy6606076@163.com>
2016-01-29 21:45:19 +0800

    +楼盘信息查看页面，CRUD方法

---
+++ Bm.Bms/Areas/Biz/Controllers/ProjectController.cs
@@ -0,0 +1,99 @@
+﻿using Bm.Modules;
+using Bm.Services.Dp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bm.Areas.Biz.Controllers
+{
+    public sealed class ProjectController : BaseAuthController
+    {
+
+        private ProjectService _service;
+
+        public ProjectController()
+        {
+            _service = new ProjectService(User?.Identity?.Name);
+        }
+        // GET: Biz/Project
+        public ActionResult Index()
+        {
+            var modes = _service.GetAll();
+            return View(modes);
+        }
+
+        // GET: Biz/Project/Details/5
+        public ActionResult Details()
+        {
+            return View();
+        }
+
+        // GET: Biz/Project/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Biz/Project/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Biz/Project/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Biz/Project/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Biz/Project/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Biz/Project/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Bm.Pub/Services/Dp/ProjectService.cs
@@ -0,0 +1,104 @@
+﻿using Bm.Models.Common;
+using Bm.Models.Dp;
+using Bm.Modules.Orm;
+using Bm.Modules.Orm.Sql;
+using Bm.Services.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bm.Services.Dp
+{
+    public sealed class ProjectService : RepoService<Project>
+    {
+        public ProjectService(string accountNo) : base(accountNo)
+        {
+
+        }
+        /// <summary>
+        /// 页面列表查看
+        /// </summary>
+        /// <returns></returns>
+        public override IList<Project> GetAll()
+        {
+            using (var conn = ConnectionManager.Open())
+            {
+                var query = new Criteria<Project>()
+                    .And(m => m.Id, Op.Gt, 0)
+                    .And(m => m.Id, Op.NotIn, new long[] { 0, -1 })
+                    .Desc(m => m.No);
+                return conn.Query(query);
+            }
+        }
+
+        /// <summary>
+        /// 添加
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public override MessageRecorder<bool> Create(Project model)
+        {
+            var r = new MessageRecorder<bool>();
+            model.CreatedAt = Now;
+            model.CreatedBy = AccountNo;
+            using (var conn = ConnectionManager.Open())
+            {
+                var trans = conn.BeginTransaction();
+                var query = new Criteria<Project>()
+                    .And(m => m.No, Op.Gt, model.No)
+                    .And(m => m.Name, Op.Gt, model.Name)
+                    .Desc(m => m.No);
+                if (conn.Exists(query))
+                {
+                    trans.Rollback();
+                    return r.Error("保存失败");
+                }
+                var effectedCount = conn.Insert(model, trans);
+                if (effectedCount == -1)
+                {
+                    trans.Rollback();
+                    return r.Error("保存失败");
+                }
+                trans.Commit();
+                return r.SetValue(true);
+            }
+
+        }
+        /// <summary>
+        /// 修改
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public override MessageRecorder<bool> Update(Project model)
+        {
+            var r = new MessageRecorder<bool>();
+            model.CreatedAt = Now;
+            model.CreatedBy = AccountNo;
+
+            using (var conn=ConnectionManager.Open())
+            {
+                var trans = conn.BeginTransaction(); 
+                var query = new Criteria<Project>()
+                    .Where(m => m.No, Op.Eq, model.No)
+                    .Or(m => m.Name, Op.Eq, model.Name)
+                    .And(m => m.Id, Op.NotEq, model.Id);
+                if (conn.Exists(query))
+                {
+                    trans.Rollback();
+                    return r.Error("编号或者名称重复");
+                }
+                var effectedCount = conn.Update(model, trans);
+                if (!effectedCount)
+                {
+                    trans.Rollback();
+                    return r.Error("保存失败");
+                }
+                trans.Commit();
+                return r.SetValue(true);
+            }
+        }
+    }
+}
+

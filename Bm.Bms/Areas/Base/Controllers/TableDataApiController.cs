d48a74bed7444178312a98cf6d580130e0822fa3
sulei <1134283134@qq.com>
2016-02-21 20:11:09 +0800

    + 加数据API控制器

---
+++ Bm.Bms/Areas/Base/Controllers/TableDataApiController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Bm.Modules.Orm;
+using com.senlang.Sdip.Util;
+using Dapper;
+
+namespace Bm.Areas.Base.Controllers
+{
+    public class TableDataApiController : Controller
+    {
+        /// <summary>
+        /// 读取制定表中一条数据
+        /// </summary>
+        /// <param name="tableName">表名</param>
+        /// <param name="no">要显示的编号</param>
+        /// <param name="name">要显示的名称</param>
+        /// <returns></returns>
+        public SelectList GetDataByTable(string tableName,string no="",string name="")
+        {
+            using (var conn = ConnectionManager.Open())
+            {
+                string sql = $"select {no},{name} from {tableName}";
+                var data = conn.Query(sql).ToList();
+                return new SelectList(data,no,$"[{no}]+{name}");
+            }
+        }
+    }
+}

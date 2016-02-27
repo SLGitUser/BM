f50cf272b2eec98ced91a20525373f3bcb339aeb
zzyao <zzy6606076@163.com>
2016-02-27 16:01:31 +0800

    *楼盘户型增删改查

---
+++ Bm.Bms/Areas/Biz/Controllers/DevRoomController.cs
@@ -0,0 +1,157 @@
+﻿using Bm.Modules;
+using Bm.Services.Dp;
+using System.Web.Mvc;
+using Bm.Models.Dp;
+using Bm.Modules.Helper;
+using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Web.Routing;
+using Bm.Extensions;
+using Bm.Services.Common;
+
+namespace Bm.Areas.Biz.Controllers
+{
+    [DisplayName("户型信息")]
+    public sealed class DevRoomController : BaseAuthController
+    {
+        private DevRoomService _service;
+
+        protected override void Initialize(RequestContext requestContext)
+        {
+            base.Initialize(requestContext);
+            _service = new DevRoomService(CurrAccountNo);
+        }
+
+        // GET: Biz/Project
+        public ActionResult Index(string No)
+        {
+            var models = _service.GetAll(No);
+            return View(models);
+        }
+
+        // GET: Biz/Project/Details/5
+        public ActionResult Details(long[] ids)
+        {
+            if (ids.IsNullOrEmpty())
+            {
+                FlashWarn("请选择一条数据");
+                return RedirectToAction("Index");
+            }
+            var list = _service.GetByIds(ids);;
+            return View(list);
+        }
+
+        // GET: Biz/Project/Create
+        public ActionResult Create(string DpNo)
+        {
+            var dev = new DevRoom{DpNo=DpNo };
+            return View(dev);
+        }
+
+        // POST: Biz/Project/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                var model = new DevRoom();
+                model.CreatedAt = DateTime.Now;
+                model.CreatedBy = "SYSTEM";
+                TryUpdateModel(model, collection);
+                if (!ModelState.IsValid)
+                {
+                    var a = GetModelErrorMsg();
+                    FlashError("数据验证未通过，请检查是否存在为空的必填项");
+                    return View(model);
+                }
+                
+                var r = _service.Create(model);
+                if (r.HasError)
+                {
+                    FlashMessage(r);
+                    return View(model);
+                }
+                return RedirectToAction("Index",new { No=model.DpNo});
+            }
+            catch (Exception e)
+            {
+                return View();
+            }
+        }
+
+        // GET: Biz/Project/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id.HasValue)
+            {
+                var model = _service.GetById(id.Value);
+                return View(model);
+            }
+            FlashWarn("没有指定ID");
+            return RedirectToAction("Index");
+        }
+
+        // POST: Biz/Project/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            var model = new DevRoom();
+            TryUpdateModel(model, collection);
+            if (!ModelState.IsValid)
+            {
+                FlashError("数据验证未通过，请检查是否存在为空的必填项");
+                return View(model);
+            }
+            model.UpdatedBy = "SYSTEM";
+            model.UpdatedAt = DateTime.Now;
+            var r = _service.Update(model);
+            if (r.HasError)
+            {
+                FlashMessage(r);
+                return View(model);
+            }
+            return RedirectToAction("Index",new { No=model.DpNo});
+        }
+
+        // GET: Biz/Project/Delete/5
+        public ActionResult Delete(long[] ids)
+        {
+            if (ids.IsNullOrEmpty())
+            {
+                FlashWarn("请选择一条数据");
+                return RedirectToAction("Index");
+            }
+            var list = _service.GetByIds(ids);
+            return View(list);
+        }
+
+        // POST: Biz/Project/Delete/5
+        [HttpPost]
+        public ActionResult Delete(FormCollection collection, long[] ids)
+        {
+            if (ids.IsNullOrEmpty())
+            {
+                FlashWarn("请选择一条数据");
+                return View();
+            }
+            var list = _service.GetByIds(ids);
+            try
+            {
+                var r = _service.Delete(list.ToArray());
+                if (r.HasError)
+                {
+                    FlashMessage(r);
+                    return View(list);
+                }
+                FlashSuccess("删除成功");
+                return RedirectToAction("Index",new {No=list.First(m=>m.Id==ids[0]).DpNo});
+            }
+            catch (Exception e)
+            {
+                FlashError("删除失败");
+                return View(list);
+            }
+        }
+    }
+}
---
+++ Bm.Pub/Services/Dp/DevRoomService.cs
@@ -0,0 +1,124 @@
+﻿using Bm.Models.Common;
+using Bm.Models.Dp;
+using Bm.Modules.Orm;
+using Bm.Modules.Orm.Sql;
+using Bm.Services.Common;
+using System.Collections.Generic;
+
+namespace Bm.Services.Dp
+{
+    public sealed class DevRoomService : RepoService<DevRoom>
+    {
+        public DevRoomService(string accountNo) : base(accountNo)
+        {
+
+        }
+        #region 模型操作
+
+        /// <summary>
+        /// 页面列表查看
+        /// </summary>
+        /// <returns></returns>
+        public  IList<DevRoom> GetAll(string ProjectNo)
+        {
+            using (var conn = ConnectionManager.Open())
+            {
+                var query = new Criteria<DevRoom>()
+                    .Where(m=>m.DpNo, Op.Eq,ProjectNo)
+                    .Desc(m => m.No);
+                return conn.Query(query);
+            }
+        }
+
+        /// <summary>
+        /// 添加
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public override MessageRecorder<bool> Create(DevRoom model)
+        {
+            var r = new MessageRecorder<bool>();
+            model.CreatedAt = Now;
+            model.CreatedBy = AccountNo;
+            using (var conn = ConnectionManager.Open())
+            {
+                var trans = conn.BeginTransaction();
+                var query = new Criteria<DevRoom>()
+                    .Where(m => m.No, Op.Eq, model.No)
+                    .Or(m => m.Name, Op.Eq, model.Name)
+                    .Desc(m => m.No);
+                if (conn.Exists(query))
+                {
+                    trans.Rollback();
+                    return r.Error("保存失败");
+                }
+                var effectedCount = conn.Insert(model, trans);
+                if (effectedCount == -1)
+                {
+                    trans.Rollback();
+                    return r.Error("保存失败");
+                }
+                trans.Commit();
+            }
+            r.Append(AccessoryService.ClearExpiration(model.Pic));
+            return r.SetValue(!r.HasError);
+
+        }
+        /// <summary>
+        /// 修改
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public override MessageRecorder<bool> Update(DevRoom model)
+        {
+            var r = new MessageRecorder<bool>();
+            model.CreatedAt = Now;
+            model.CreatedBy = AccountNo;
+            string oldKey;
+            using (var conn = ConnectionManager.Open())
+            {
+                var trans = conn.BeginTransaction();
+                var query = new Criteria<DevRoom>()
+                    .Where(m => m.No, Op.Eq, model.No)
+                    .Or(m => m.Name, Op.Eq, model.Name)
+                    .And(m => m.Id, Op.NotEq, model.Id);
+                if (conn.Exists(query))
+                {
+                    trans.Rollback();
+                    return r.Error("编号或者名称重复");
+                }
+                var obj = new Criteria<DevRoom>()
+                    .Where(m => m.Id, Op.Eq, model.Id)
+                    .Select(m => m.Pic);
+                oldKey = conn.ExecuteScalarEx<string>(obj.ToSelectSql());
+
+                var effectedCount = conn.Update(model, trans);
+                if (!effectedCount)
+                {
+                    trans.Rollback();
+                    return r.Error("保存失败");
+                }
+                trans.Commit();
+            }
+            if (!Equals(oldKey, model.Pic))
+            {
+                r.Append(AccessoryService.DeleteObject(oldKey));
+                r.Append(AccessoryService.ClearExpiration(model.Pic));
+            }
+            return r.SetValue(!r.HasError);
+        }
+
+
+        public override MessageRecorder<bool> Delete(params DevRoom[] models)
+        {
+            var r = base.Delete(models);
+            foreach (var model in models)
+            {
+                r.Append(AccessoryService.DeleteObject(model.Pic));
+            }
+            return r;
+        }
+        #endregion
+    }
+}
+

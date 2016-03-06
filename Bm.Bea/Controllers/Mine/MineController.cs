74aa34a703732cc821bda7b5e897c8b569389424
sulei <1134283134@qq.com>
2016-03-06 15:22:44 +0800

    *

--- Bm.Bea/Controllers/Mine/MineController.cs
+++ Bm.Bea/Controllers/Mine/MineController.cs
@@ -12,32 +12,6 @@ namespace Bm.Controllers.Mine
 
     public class MineController : BaseApiController
     {
-
-        /// <summary>
-        /// 获取所有房源信息
-        /// </summary>
-        /// <returns></returns>
-        [Route("api/get_house_all")]
-        public IHttpActionResult GetHouseAll()
-        {
-            var service = new ProjectService();
-            var r = service.GetAllHouse();
-            return Ok(r);
-        }
-        /// <summary>
-        /// 获取我的房源信息
-        /// </summary>
-        /// <returns></returns>
-        [Route("api/get_house_my")]
-        public IHttpActionResult GetHouseMy()
-        {
-            //var m = Request.GetQueryString("m");
-            var u = Request.GetQueryString("u");
-
-            var service = new HouseBrokerRefService();
-            var r = service.GetHouseByBrokerNo(u);
-            return Ok(r);
-        }
         /// <summary>
         /// 根据经纪人编号获取公司信息
         /// </summary>

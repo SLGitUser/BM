ffa04a871454794dcaac3def74e8c08d66350451
cyuil <952589446@qq.com>
2016-03-06 19:30:17 +0800

    +

--- Bm.Bea/Controllers/Mine/MineController.cs
+++ Bm.Bea/Controllers/Mine/MineController.cs
@@ -6,6 +6,7 @@ using Bm.Models.Common;
 using Bm.Models.Dp;
 using Bm.Modules.Helper;
 using Bm.Services.Dp;
+using System.Collections.Generic;
 
 namespace Bm.Controllers.Mine
 {
@@ -24,6 +25,34 @@ namespace Bm.Controllers.Mine
             var service = new BrokerService();
             var r = service.GetByNo(u);
             return Ok(r);
+        }
+        /// <summary>
+        /// 获取所有公司信息
+        /// </summary>
+        /// <returns></returns>
+        [Route("api/get_mine_firmall")]
+        public IHttpActionResult GetFirmAll()
+        {
+            var r = new MessageRecorder<IList<BrokerageFirm>>();
+            var u = Request.GetQueryString("u");
+            var service = new BrokerageFirmService(u);
+            r.SetValue(service.GetAll());
+            return Ok(r);
+        }
+        /// <summary>
+        /// 绑定公司
+        /// </summary>
+        /// <returns></returns>
+        [Route("api/get_mine_save")]
+        [HttpGet]
+        public IHttpActionResult SetFirm()
+        {
+            var r = new MessageRecorder<IList<BrokerageFirm>>();
+            var u = Request.GetQueryString("u");
+            var n = Request.GetQueryString("n");
+            var service = new BrokerService();
+            var a = service.UpdateFirm(u,n);
+            return Ok(a);
         }
     }
 }
